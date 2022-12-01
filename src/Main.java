cf303f105ae713c0b757ef3d7855c72442b7152b
alkor63 <alkor@medi-spa.ru>
2022-12-01 12:55:36 +0300

    Сделано ДЗ: Приветствуем мир по-русски

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
+    public static double square(double x) {
+        double y;
+        y = x*x;
+        return y;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        double num = 1.;
+        Scanner in = new Scanner(System.in);
+        System.out.println("При вводе double в качестве разделителя используйте запятую");
+        while (num != 0) {
+        System.out.print("Введите число: ");
+                num = in.nextDouble();
+        System.out.println(num +" в квадрате = " + square(num));
+        System.out.println("(для выхода из программы введите '0')");
+        }
+        in.close();
     }
 }
